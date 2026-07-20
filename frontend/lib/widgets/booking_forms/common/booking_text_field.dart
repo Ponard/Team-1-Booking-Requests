e6f7f67f7cd4e680583bc6fa58a7022f8989cf1d
Cyrus John Ponard <159444800+Ponard@users.noreply.github.com>
2026-07-20 15:43:07 +0800

    refactor(frontend): register booking fields with form controller

--- frontend/lib/widgets/booking_forms/common/booking_text_field.dart
+++ frontend/lib/widgets/booking_forms/common/booking_text_field.dart
@@ -1,6 +1,9 @@
+import 'package:diocese_frontend/widgets/booking_forms/form/booking_field_handle.dart';
+import 'package:diocese_frontend/widgets/booking_forms/form/booking_form_controller.dart';
+import 'package:diocese_frontend/widgets/booking_forms/form/booking_form_scope.dart';
 import 'package:flutter/material.dart';
 
-class BookingTextField extends StatelessWidget {
+class BookingTextField extends StatefulWidget {
   final TextEditingController controller;
 
   final String label;
@@ -21,6 +24,9 @@ class BookingTextField extends StatelessWidget {
 
   final FormFieldValidator<String>? validator;
 
+  @override
+  State<BookingTextField> createState() => _BookingTextFieldState();
+
   const BookingTextField({
     super.key,
     required this.controller,
@@ -35,7 +41,6 @@ class BookingTextField extends StatelessWidget {
     this.validator,
   });
 
-  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 12),
@@ -52,6 +57,61 @@ class BookingTextField extends StatelessWidget {
           hintText: hint,
           border: const OutlineInputBorder(),
           suffixIcon: suffixIcon,
+        ),
+      ),
+    );
+  }
+}
+
+class _BookingTextFieldState extends State<BookingTextField> {
+  late final BookingFieldHandle _field;
+  BookingFormController? _controller;
+  bool _registered = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _field = BookingFieldHandle();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    if (!_registered) {
+      _controller = BookingFormScope.of(context);
+      _controller!.register(_field);
+      _registered = true;
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller?.unregister(_field);
+    _field.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12),
+      child: TextFormField(
+        key: _field.key,
+        focusNode: _field.focusNode,
+        controller: widget.controller,
+        enabled: widget.enabled,
+        readOnly: widget.readOnly,
+        keyboardType: widget.keyboardType,
+        maxLines: widget.maxLines,
+        validator: widget.validator,
+        onTap: widget.onTap,
+        decoration: InputDecoration(
+          labelText: widget.label,
+          hintText: widget.hint,
+          border: const OutlineInputBorder(),
+          suffixIcon: widget.suffixIcon,
         ),
       ),
     );
